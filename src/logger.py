8cf37dbdb0c357b2c889c49fe0cc52ee655e0c57
aastha <aasthasonu28@gmail.com>
2024-07-14 12:48:58 +0530

    EDA

--- src/logger.py
+++ src/logger.py
@@ -2,11 +2,10 @@ import logging
 import os
 from datetime import datetime
 
-log_file=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",log_file)
-os.makedirs(logs_path,exist_ok=True)
-
-log_file_path=os.path.join(logs_path,log_file)
+log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
+log_file_path = os.path.join(logs_path, log_file)
 
 logging.basicConfig(
     filename=log_file_path,
@@ -14,6 +13,5 @@ logging.basicConfig(
     level=logging.INFO,
 )
 '''
-if __name__=="__main__":
-    logging.info("logging has started")
+logging.info("Logging configuration setup complete")
 '''
